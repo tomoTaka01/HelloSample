12827a585cdef3d9e628a53ea726c64e7f058dc0
tomo taka <tomo_taka01@hotmail.com>
2013-01-05 20:17:08 +0900

    add javascript

---
+++ build/web/resource/base.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function clear() {
+    if (confirm("Are you sure?")) {
+        form1.getElementById("form1:name").text = "";
+    }
+}
---
+++ build/web/resources/js/base.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function clearName() {
+    if (confirm("Are you sure?")) {
+        document.getElementById("form1:name").value = "";
+    }
+}
